b3f713490dcf6d73c5242bc4092d003e78b64fc5
kendo5731 <aur.baumann@gmail.com>
2014-07-15 14:15:01 +0200

    Add todo_list exercice

---
+++ todo_list/js/app.js
@@ -0,0 +1,4 @@
+var todoTemplate = '<tr class="todo" id="todo-{{id}}"><td>{{id}}</td><td>{{content}}</td><td><a href="#" class="checkout"><i class="glyphicon glyphicon-ok"></i></a></td><td><a href="#" class="delete btn btn-danger">Delete</a></td></tr>';
+var doneTemplate = '<tr class="todo" id="todo-{{id}}"><td>{{id}}</td><td>{{content}}</td><td><i class="glyphicon glyphicon-ok"></i></td></tr>';
+
+// Your code here ...
